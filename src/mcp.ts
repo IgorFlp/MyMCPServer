65d1cc920899245507f30b8db258866e941c6ab7
IgorFlp <igor.felipe.13@hotmail.com>
2026-06-16 22:08:10 -0300

    Added MCP message encryption

--- src/mcp.ts
+++ src/mcp.ts
@@ -7,3 +7,37 @@ export const server = new McpServer({
     version: '0.0.1'
 })
 
+server.registerTool(
+    'encrypt_message',
+    {
+        description: 'Encrypt a message',
+        inputSchema:{
+            message: z.string().describe("Message to encrypt"),
+            encryptionKey: z.string().describe("Any passpharase to use for encryption")
+        },
+        outputSchema:{
+            encryptedMessage: z.string().describe(
+                "Encrypted message format: iv:ciphertext"
+            )
+        }
+    },
+    async ({message,encryptionKey})=>{
+        try {
+            const encryptedMessage = encrypt(message, encryptionKey)
+            return {
+                content: [{type: 'text', text:'encryptedMessage'}],
+                structuredContent: {encryptedMessage}
+            }
+        } catch (err) {
+            return{
+                isError: true,
+                content:[
+                    {
+                        type: 'text',
+                        text: `Failed to encrypt, please check message and encryption key: ${err instanceof Error? err.message : String(err)}`
+                    }
+                ]
+            }
+        }
+    }
+)
